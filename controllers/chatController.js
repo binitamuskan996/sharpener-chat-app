7703a31eaf2993682e599485eee3eb8f5fd4b46a
binitamuskan996 <binitabini05@gmail.com>
2026-01-19 17:01:07 +0530

    fetch messages

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,11 +4,11 @@ const User = require('../models/userModel')
 const sendMessage = async (req, res) => {
   const { message } = req.body;
   const userId = req.user.id;
-
+console.log(userId)
   if (!message) return res.status(400).json({ message: "Message is empty" });
 
   try {
-    const msg = await Message.create({ userId, message });
+    const msg = await Message.create({ UserId:userId, message });
     res.status(201).json({ success: true, message: msg });
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -19,11 +19,21 @@ const getMessages = async (req, res) => {
   try {
     const messages = await Message.findAll({
       order: [["createdAt", "ASC"]],
-      include: { model: User, attributes: ["name"] }
+      include: { model: User, attributes: ["id", "name"] }
     });
-    res.json(messages);
+    const formattedMessages = messages.map(msg => ({
+      id: msg.id,
+      message: msg.message,
+      createdAt: msg.createdAt,
+      userId: msg.UserId,      
+      username: msg.User.name ||null 
+    }));
+
+    res.json(formattedMessages);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
+
 module.exports = { getMessages, sendMessage }
