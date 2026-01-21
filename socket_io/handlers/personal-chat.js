fac1f5b3856366ac8c6c4cabecdc52219a595067
binitamuskan996 <binitabini05@gmail.com>
2026-01-21 16:19:56 +0530

    add personal message socket.io code client side 2

--- socket_io/handlers/personal-chat.js
+++ socket_io/handlers/personal-chat.js
@@ -1,13 +1,16 @@
 module.exports = (socket, io) => {
-  
+
   socket.on("join-room", (roomName) => {
     socket.join(roomName);
-    console.log(`User ${socket.user.name} joined room: ${roomName}`);
+    console.log(`User ${socket.user.name} joined room ${roomName}`);
   });
 
-  socket.on("new-message", ({message, roomName}) => {
-    console.log("User", socket.user.username, "said:", message);
-
-    io.emit("new-message", { username: socket.user.username, message });
+  socket.on("new-message", ({ message, roomName }) => {
+    io.to(roomName).emit("new-message", {
+      senderId: socket.user.id,
+      senderName: socket.user.name,
+      message
+    });
   });
+
 };
